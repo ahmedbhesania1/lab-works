05a632b101c7295dc6299f6a0c043f00c9d2f07d
ahmedbhesania1 <ahmedpa950@gmail.com>
2026-07-24 20:38:11 +0530

    add new file

---
+++ lab_work_2.2.py
@@ -0,0 +1,32 @@
+print("ahgmed")
+# Q-1
+num = int(input("enter a number: "))
+
+if num % 2==0:
+    print("even")
+else:
+    print("odd")
+
+# Q-2 to be learned
+
+# Q-3
+num1=int(input("enter 1st number: "))
+num2=int(input("enter 2nd number: "))
+num3=int(input("enter 3rd number: "))
+
+if num1>=num2 and num1>=num3:
+    print(f"{num1} is greatest")
+elif num2>=num3:
+    print(f"{num2} is greatest")
+else:
+    print(f"{num3} is greatest")
+
+# Q-4
+num=int(input("enter a number: "))
+if num>0:
+    print("positive")
+elif num<0:
+    print("negative")
+else:
+    print("neutral")
+    
